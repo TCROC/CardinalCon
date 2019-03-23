b134f2f3c044b2d847c4f021b7b595ccc714b94e
Travis Lange <travislange12@gmail.com>
2019-03-23 14:15:22 -0400

    Created a script for pingponging between points and lerping from one point to another

---
+++ Assets/BeatMatch/Scripts/Beat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Assets/BeatMatch/Scripts/BeatMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BeatMovement : MonoBehaviour
+{
+    public Transform endPoint;
+
+    public float timeToComplete;
+
+    public AnimationCurve animationCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0, 1));
+
+    public UnityEvent onAnimationComplete;
+
+    private void Start()
+    {
+        StartCoroutine(LerpToPoint(endPoint));
+    }
+
+    IEnumerator LerpToPoint(Transform point)
+    {
+        float startTime = Time.time;
+
+        float percentageComplete = 0;
+        Vector3 startPos = transform.position;
+
+        while (percentageComplete < 1)
+        {
+            transform.position = Vector3.LerpUnclamped(startPos, point.position, percentageComplete);
+            percentageComplete = animationCurve.Evaluate(Time.time - startTime);
+            yield return null;
+        }
+
+        onAnimationComplete?.Invoke();
+    }
+}
