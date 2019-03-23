d79eea59329aaa57d912d28f59de08494bc0e147
Travis Lange <travislange12@gmail.com>
2019-03-23 17:03:47 -0400

    The beat style things now works with time and scoring

--- Assets/BeatMatch/Scripts/Beat.cs
+++ Assets/BeatMatch/Scripts/Beat.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 
 public class Beat : MonoBehaviour
 {
+    public static float beatSpeed = 2;
+
     public int explosionCount;
     public GameObject explodedCubePrefab;
 
     private void Update()
     {
-        Collider[] colliders = Physics.OverlapBox(transform.position, transform.lossyScale / 2);
-        if (colliders?.Length > 0 && colliders.Any(i => i.tag == "Player"))
+        Collider col = Physics.OverlapBox(transform.position, transform.lossyScale / 2)?.FirstOrDefault(i => i.tag == "Player");
+        if (col != null)
         {
+            if (col.gameObject.name == "Player1")
+            {
+                BeatGameManager.instance.P1Score++;
+            }
+            else if (col.gameObject.name == "Player2")
+            {
+                BeatGameManager.instance.P2Score++;
+            }
+
             for (int i = 0; i < explosionCount; i++)
             {
                 GameObject go = Instantiate(explodedCubePrefab, transform.position, Quaternion.identity);
@@ -20,5 +31,7 @@ public class Beat : MonoBehaviour
             }
             Destroy(gameObject);
         }
+
+        transform.Translate(Vector3.back * beatSpeed * Time.smoothDeltaTime);
     }
 }
---
+++ Assets/BeatMatch/Scripts/BeatGameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class BeatGameManager : MonoBehaviour
+{
+    public static BeatGameManager instance;
+
+    public int P1Score
+    {
+        get
+        {
+            return p1Score;
+        }
+        set
+        {
+            p1Score = value;
+            tmpP1Score.text = p1Score.ToString();
+        }
+    }
+
+    public int P2Score
+    {
+        get
+        {
+            return p2Score;
+        }
+        set
+        {
+            p2Score = value;
+            tmpP2Score.text = p2Score.ToString();
+        }
+    }
+
+    private int p1Score;
+    private int p2Score;
+
+    [SerializeField]
+    private int timeLeft = 180;
+
+    [SerializeField]
+    private TextMeshProUGUI tmpP1Score;
+
+    [SerializeField]
+    private TextMeshProUGUI tmpP2Score;
+
+    [SerializeField]
+    private TextMeshProUGUI tmpTimeLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            timeLeft--;
+
+            var span = new TimeSpan(0, 0, timeLeft);
+            var formattedTime = string.Format("{0}:{1:00}", (int)span.TotalMinutes, span.Seconds);
+
+            int seconds = timeLeft % 60;
+            int minutes = timeLeft / 60;
+            string time = minutes.ToString() + ":" + seconds.ToString();
+
+            tmpTimeLeft.text = formattedTime;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
