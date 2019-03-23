5c88319240f0672e3d022ae22af9a0b010fddd7a
Travis Lange <travislange12@gmail.com>
2019-03-23 15:34:45 -0400

    Created the core system for slicing the cubes

--- Assets/BeatMatch/Scripts/Beat.cs
+++ Assets/BeatMatch/Scripts/Beat.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Beat : MonoBehaviour
 {
-    public void OnCollisionEnter(Collision collision)
+    public int explosionCount;
+    public GameObject explodedCubePrefab;
+
+    private void Update()
     {
-        
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.lossyScale / 2);
+        if (colliders?.Length > 0 && colliders.Any(i => i.tag == "Player"))
+        {
+            for (int i = 0; i < explosionCount; i++)
+            {
+                GameObject go = Instantiate(explodedCubePrefab, transform.position, Quaternion.identity);
+                Destroy(go, 2);
+            }
+            Destroy(gameObject);
+        }
     }
 }
