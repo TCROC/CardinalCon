082f7bd04d71af6812a9a3b2c27221d06755bed7
Kailee Russell <48765318+knrussel1@users.noreply.github.com>
2019-03-25 13:18:28 -0400

    1:18pm 3/25

---
+++ Assets/Split_Screen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Split_Screen : MonoBehaviour
+{
+    public Transform mainCamera;
+
+    void cameraFollow()
+    {
+        float charPosX = transform.position.x;
+        float charPosZ = transform.position.z;
+        float cameraOffset = 18.0f;
+
+        this.transform.position = new Vector3(charPosX, cameraOffset, charPosX);
+        
+
+
+    }
+
+    private void Update()
+    {
+        cameraFollow();
+    }
+
+
+}
