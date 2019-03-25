055284c7359350e5947f93002fe3ed3eeae832ed
Kailee Russell <48765318+knrussel1@users.noreply.github.com>
2019-03-25 13:15:16 -0400

    1:11pm 3/25 [FIXED AND UPDATED]
    A friend helped with the camera script. I feel as if it should be a time trial race, so the players are competing for the best time. I;m going to figure out a GUI. I will fix up the level for a better design and feel.  Let me know what you think.

---
+++ Assets/Split_Screen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Split_Screen : MonoBehaviour
+{
+    public Transform mainCamera;
+
+    void cameraFollow()
+    {
+        float charPosX = transform.position.x;
+        float charPosZ = transform.position.z;
+        float cameraOffset = 18.0f;
+
+        mainCamera.transform.position = new Vector3(charPosX, cameraOffset, charPosX);
+        mainCamera.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+
+    }
+
+    private void Update()
+    {
+        cameraFollow();
+    }
+
+
+}
