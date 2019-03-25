2815afea4a95022ef99ca3d3083f78b117b7c148
Kailee Russell <48765318+knrussel1@users.noreply.github.com>
2019-03-25 12:56:40 -0400

    12:56pm 3/25
    camera update FIXED

---
+++ Assets/camerabehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerabehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float xOffset = 0f;
+    public float yOffset = 0f;
+    public float zOffset = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(target.transform.position.x + xOffset,
+                                                target.transform.position.y + yOffset,
+                                                target.transform.position.z + zOffset);
+        this.transform.rotation = Quaternion.LookRotation(target.position - transform.position, -Vector3.down);
+    }
+}
